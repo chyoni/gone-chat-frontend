d3e912bfa12640db71e25c76f2728c2008970f6b
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-09-22 21:31:05 +0900

    #01 Init

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export const App: React.FC<{}> = () => {
+  return (
+    <div className="App">
+      <h1>GONE-CHAT-FRONTEND</h1>
+    </div>
+  );
+};
